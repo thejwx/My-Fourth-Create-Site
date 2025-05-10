7348457551c87c5771f50de9ba9c972a01054432
Jeff Wilson <whackscript@gmail.com>
2025-05-10 18:02:51 -0500

    Convert to Next.js project

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="p-4">
+      <h1 className="site-title">My Next.js Site!</h1>
+      <p className="site-tagline">Converted to Next.js framework.</p>
+      <nav className="mt-2 space-x-4">
+        <a className="nav-link" href="/">Home</a>
+        <a className="nav-link" href="/about">About</a>
+        <a className="nav-link" href="/style-guide">Style Guide</a>
+      </nav>
+    </header>
+  )
+}
