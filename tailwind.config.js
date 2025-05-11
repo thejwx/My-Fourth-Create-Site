e700dcc2a1988852ba5d14b244a6fa44c4ee198c
Jeff Wilson <whackscript@gmail.com>
2025-05-10 19:01:01 -0500

    Convert to Next.js project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+  "brand": "#8B3A3A",
+  "accent": "#DAA520",
+  "neutral": "#372323"
+},
+      fontFamily: {
+  "heading": "Ysabeau Office",
+  "body": "Inter"
+}
+    },
+  },
+  plugins: [],
+}
